583871517fc8de68c276e2db4c475897acbcf2d7
ZuPerfect <315698477@qq.com>
2023-08-21 18:44:15 +0800

    perf: 计算中心点

---
+++ src/components/CabinetPlacement.vue
@@ -0,0 +1,241 @@
+<!--
+ * @描 述: 继续threejs实现的柜体摆放案例
+ * @作 者: 朱鹏飞
+-->
+
+<template></template>
+
+<script>
+import { onMounted } from "vue";
+import * as THREE from "three";
+import Stats from "three/addons/libs/stats.module.js";
+import { OBJLoader } from "three/addons/loaders/OBJLoader";
+import { addStats, addOrbitControls, addAxesHelper, addDirectionalLight } from "../js/common.js";
+import { DragControls } from "three/addons/controls/DragControls.js";
+import { CSS2DRenderer, CSS2DObject } from "three/addons/renderers/CSS2DRenderer.js";
+
+// 定义一些常量
+const stats = new Stats();
+const width = window.innerWidth;
+const height = window.innerHeight;
+const obj1Path = new URL("../assets/objs/small cabinet for program v2.obj", import.meta.url).href;
+const obj2Path = new URL("../assets/objs/wide wide cabinet for program v2.obj", import.meta.url).href;
+
+const loaderObj = objPath => {
+  return new Promise((resolve, reject) => {
+    const loader = new OBJLoader();
+    loader.load(objPath, obj => {
+      resolve(obj);
+    });
+  });
+};
+
+export default {
+  name: "CabinetPlacement",
+  setup() {
+    // 创建一个三维场景对象
+    const scene = new THREE.Scene();
+
+    // 漫反射网格材质（受光照的影响）
+    const material = new THREE.MeshLambertMaterial({
+      color: 0xff00ff,
+      // side: THREE.DoubleSide,
+      transparent: true,
+      opacity: 0.8,
+      // map: getTexture("中心矩阵"),
+    });
+
+    /*--------------------obj模型加载--------------------*/
+    //纹理贴图加载器TextureLoader
+    const loadObj = () => {
+      let dragControls1 = null;
+      let dragControls2 = null;
+      const obj1Loader = loaderObj(obj1Path);
+      const obj2Loader = loaderObj(obj2Path);
+      Promise.all([obj1Loader, obj2Loader]).then(groups => {
+        groups[0].traverse(obj => {
+          if (obj.isMesh) {
+            obj.position.y += 330;
+          }
+        });
+        groups.forEach(group => {
+          group.traverse(obj => {
+            if (obj.isMesh) {
+              obj.rotation.x += -Math.PI / 2;
+              const div = document.createElement("div");
+              div.innerHTML = obj.name;
+              const tag = new CSS2DObject(div);
+              const geometry = obj.geometry;
+
+              // 获取模型的顶点坐标
+              const vertices = geometry.attributes.position.array;
+
+              obj.updateMatrixWorld(true);
+              // 初始化中心点坐标
+              const center = new THREE.Vector3();
+              obj.geometry.computeBoundingBox();
+              obj.geometry.boundingBox.getCenter(center);
+              center.applyMatrix4(obj.matrixWorld);
+              // // 遍历顶点坐标，累加每个顶点的坐标值
+              // for (let i = 0; i < vertices.length; i += 3) {
+              //   center.x += vertices[i];
+              //   center.y += vertices[i + 1];
+              //   center.z += vertices[i + 2];
+              // }
+
+              // // 求取平均值
+              // const vertexCount = vertices.length / 3;
+              // center.divideScalar(vertexCount);
+              // const center = new THREE.Vector3();
+              // geometry.computeBoundingBox();
+              // geometry.boundingBox.getCenter(center);
+              // geometry.applyMatrix4(obj.matrixWorld);
+              // const box = new THREE.Box3();
+              // box.expandByObject(obj);
+              // 重新计算外包框的最小点和最大点
+              // box.min.applyMatrix4(obj.matrixWorld);
+              // box.max.applyMatrix4(obj.matrixWorld);
+
+              // const boxHelper = new THREE.Box3Helper(box, 0xffff00);
+              // scene.add(boxHelper);
+              // const center = new THREE.Vector3();
+              // box.getCenter(center);
+              // center.applyMatrix4(group.matrixWorld);
+              tag.position.copy(center);
+              scene.add(tag);
+            }
+          });
+        });
+
+        scene.add(...groups);
+
+        // const box = new THREE.Box3().setFromObject(groups[0].getObjectByName("mdf bottom"));
+        // const boxHelper = new THREE.Box3Helper(box, 0xffff00);
+        // scene.add(boxHelper);
+
+        // 创建拖动控制器
+        dragControls1 = new DragControls([groups[0]], camera, renderer.domElement);
+        dragControls2 = new DragControls([groups[1]], camera, renderer.domElement);
+        dragControls1.transformGroup = true;
+        dragControls2.transformGroup = true;
+
+        // 监听拖动开始事件
+        dragControls1.addEventListener("dragstart", function (event) {
+          // 停止渲染循环，防止拖动过程中产生闪烁
+          controls.enabled = false;
+          dragControls2.enabled = false;
+        });
+
+        // 监听拖动结束事件
+        dragControls1.addEventListener("dragend", function (event) {
+          // 启动渲染循环
+          controls.enabled = true;
+          dragControls2.enabled = true;
+          const group = event.object;
+          group.traverse(obj => {
+            if (obj.isMesh) {
+              const div = document.createElement("div");
+              div.innerHTML = obj.name;
+              const tag = new CSS2DObject(div);
+
+              // 初始化中心点坐标
+              const center = new THREE.Vector3();
+              obj.geometry.computeBoundingBox();
+              obj.geometry.boundingBox.getCenter(center);
+              // 获取包围盒的最小和最大顶点
+              const min = boundingBox.min;
+              const max = boundingBox.max;
+              // 计算高度、宽度和深度
+              const height = Math.abs(max.y - min.y);
+              const width = Math.abs(max.x - min.x);
+              const depth = Math.abs(max.z - min.z);
+
+              center.applyMatrix4(obj.matrixWorld);
+              tag.position.copy(center);
+
+              scene.add(tag);
+            }
+          });
+        });
+
+        // 监听拖动开始事件
+        dragControls2.addEventListener("dragstart", function (event) {
+          // 停止渲染循环，防止拖动过程中产生闪烁
+          controls.enabled = false;
+          dragControls1.enabled = false;
+        });
+
+        // 监听拖动结束事件
+        dragControls2.addEventListener("dragend", function (event) {
+          // 启动渲染循环
+          controls.enabled = true;
+          dragControls1.enabled = true;
+        });
+      });
+    };
+
+    // 创建一个透视投影的相机对象
+    const camera = new THREE.PerspectiveCamera(10, width / height, 1, 30000);
+    camera.position.set(0, 0, 2000);
+    camera.lookAt(0, 0, 0);
+
+    // 创建渲染器对象
+    const renderer = new THREE.WebGLRenderer({
+      // 锯齿模糊
+      antialias: true,
+    });
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.setSize(width, height);
+    // 设置背景颜色
+    renderer.setClearColor(0x1e1e1e, 1); //设置背景颜色
+    renderer.render(scene, camera);
+
+    /*----------------------------创建css2d渲染器对象----------------------------*/
+    const css2DRenderer = new CSS2DRenderer();
+    css2DRenderer.setSize(width, height);
+    css2DRenderer.domElement.style.position = "absolute";
+    css2DRenderer.domElement.style.top = "0px";
+    // 设置.style.pointerEvents = none，就可以解决HTML元素标签对threejs canvas画布鼠标事件的遮挡
+    css2DRenderer.domElement.style.pointerEvents = "none";
+
+    // 添加平行光
+    addDirectionalLight(scene);
+
+    // 坐标格辅助对象. 坐标格实际上是2维线数组.
+    const grid = new THREE.GridHelper(5000, 50, 0xc1c1c1, 0x8d8d8d);
+    scene.add(grid);
+
+    addAxesHelper(scene);
+
+    // 添加交互控制器
+    const controls = addOrbitControls(scene, camera, renderer);
+
+    const init = () => {
+      document.body.appendChild(renderer.domElement);
+      document.body.appendChild(css2DRenderer.domElement);
+      loadObj();
+      addStats(stats);
+      window.onresize = function () {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        css2DRenderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+      };
+    };
+
+    const render = () => {
+      stats.update();
+      renderer.render(scene, camera);
+      css2DRenderer.render(scene, camera);
+      requestAnimationFrame(render);
+    };
+
+    onMounted(() => {
+      init();
+      render();
+    });
+
+    return {};
+  },
+};
+</script>
