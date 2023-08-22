2ac368cc7d0179852d42d8b4a4f2a5f68b2de050
ZuPerfect <315698477@qq.com>
2023-08-22 19:11:34 +0800

    perf: 代码整理

--- src/components/CabinetPlacement.vue
+++ src/components/CabinetPlacement.vue
@@ -3,7 +3,9 @@
  * @作 者: 朱鹏飞
 -->
 
-<template></template>
+<template>
+  <div class="btns"></div>
+</template>
 
 <script>
 import { onMounted } from "vue";
@@ -12,7 +14,6 @@ import Stats from "three/addons/libs/stats.module.js";
 import { OBJLoader } from "three/addons/loaders/OBJLoader";
 import { addStats, addOrbitControls, addAxesHelper, addDirectionalLight } from "../js/common.js";
 import { DragControls } from "three/addons/controls/DragControls.js";
-import { CSS2DRenderer, CSS2DObject } from "three/addons/renderers/CSS2DRenderer.js";
 
 // 定义一些常量
 const stats = new Stats();
@@ -30,120 +31,116 @@ const loaderObj = objPath => {
   });
 };
 
+const disableControls = (dragControlList, currentIdx) => {
+  dragControlList.forEach((c, idx) => {
+    if (currentIdx !== idx) {
+      c.enabled = false;
+    }
+  });
+};
+
+const enableControls = dragControlList => {
+  dragControlList.forEach(c => {
+    c.enabled = true;
+  });
+};
+
 export default {
   name: "CabinetPlacement",
   setup() {
     // 创建一个三维场景对象
     const scene = new THREE.Scene();
 
-    // 漫反射网格材质（受光照的影响）
-    const material = new THREE.MeshLambertMaterial({
-      color: 0xff00ff,
-      // side: THREE.DoubleSide,
-      transparent: true,
-      opacity: 0.8,
-      // map: getTexture("中心矩阵"),
-    });
-
     /*--------------------obj模型加载--------------------*/
     //纹理贴图加载器TextureLoader
     const loadObj = () => {
-      let dragControls1 = null;
-      let dragControls2 = null;
+      const dragControlList = [];
       const obj1Loader = loaderObj(obj1Path);
       const obj2Loader = loaderObj(obj2Path);
-      Promise.all([obj1Loader, obj2Loader]).then(groups => {
-        groups[0].position.y += 330;
+      const obj3Loader = loaderObj(obj1Path);
+      Promise.all([obj1Loader, obj2Loader, obj3Loader]).then(groups => {
+        let offset = 0;
         groups.forEach(group => {
           group.rotation.x += -Math.PI / 2;
+          group.position.z += offset;
+          offset += 400;
         });
 
         scene.add(...groups);
 
-        // 创建拖动控制器
-        dragControls1 = new DragControls([groups[0]], camera, renderer.domElement);
-        dragControls2 = new DragControls([groups[1]], camera, renderer.domElement);
-        dragControls1.transformGroup = true;
-        dragControls2.transformGroup = true;
+        for (let i = 0; i < groups.length; i++) {
+          const dragControl = new DragControls([groups[i]], camera, renderer.domElement);
+          dragControl.transformGroup = true;
+          dragControlList.push(dragControl);
+        }
 
-        // 监听拖动开始事件
-        dragControls1.addEventListener("dragstart", function (event) {
-          // 停止渲染循环，防止拖动过程中产生闪烁
-          controls.enabled = false;
-          dragControls2.enabled = false;
-        });
+        dragControlList.forEach((dragControl, i) => {
+          dragControl.addEventListener("dragstart", function (event) {
+            controls.enabled = false;
+            disableControls(dragControlList, i);
+          });
+          dragControl.addEventListener("drag", function (event) {
+            controls.enabled = false;
+            disableControls(dragControlList, i);
+          });
+          dragControl.addEventListener("dragend", function (event) {
+            controls.enabled = true;
+            enableControls(dragControlList);
+            if (i === 0 || i === 2) {
+              const group = event.object;
+              const snapMesh = groups[1].getObjectByName("bottom paper (1)");
+              const center1 = new THREE.Vector3();
+              const box1 = new THREE.Box3().setFromObject(snapMesh);
+              const min = box1.min;
+              const max = box1.max;
+              // 计算每个轴的长度和一半长度
+              const sizeX = max.x - min.x;
+              const sizeY = max.y - min.y;
+              const sizeZ = max.z - min.z;
 
-        // 监听拖动结束事件
-        dragControls1.addEventListener("dragend", function (event) {
-          // 启动渲染循环
-          controls.enabled = true;
-          dragControls2.enabled = true;
-          const group = event.object;
-          const snapMesh = groups[1].getObjectByName("bottom paper (1)");
-          const center1 = new THREE.Vector3();
-          const box1 = new THREE.Box3().setFromObject(snapMesh);
-          const min = box1.min;
-          const max = box1.max;
-          // 计算每个轴的长度和一半长度
-          const sizeX = max.x - min.x;
-          const sizeY = max.y - min.y;
-          const sizeZ = max.z - min.z;
+              const leftCenter = new THREE.Vector3(
+                min.x + (sizeX / 4) * 1,
+                min.y + (sizeY / 4) * 1,
+                min.z + (sizeZ / 4) * 2
+              );
 
-          const leftCenter = new THREE.Vector3(
-            min.x + (sizeX / 4) * 1,
-            min.y + (sizeY / 4) * 1,
-            min.z + (sizeZ / 4) * 2
-          );
+              const rightCenter = new THREE.Vector3(
+                min.x + (sizeX / 4) * 3,
+                min.y + (sizeY / 4) * 3,
+                min.z + (sizeZ / 4) * 2
+              );
 
-          const rightCenter = new THREE.Vector3(
-            min.x + (sizeX / 4) * 3,
-            min.y + (sizeY / 4) * 3,
-            min.z + (sizeZ / 4) * 2
-          );
+              box1.getCenter(center1);
 
-          box1.getCenter(center1);
+              const mesh = group.getObjectByName("paper (1)");
+              const center2 = new THREE.Vector3();
+              const box2 = new THREE.Box3().setFromObject(mesh);
+              box2.getCenter(center2);
 
-          const mesh = group.getObjectByName("paper (1)");
-          const center2 = new THREE.Vector3();
-          const box2 = new THREE.Box3().setFromObject(mesh);
-          box2.getCenter(center2);
-
-          if (center1.distanceTo(center2) < 200) {
-            const offsetX = center2.x - center1.x;
-            const offsetY = center2.y - center1.y;
-            const offsetZ = center2.z - center1.z;
-            group.position.x -= offsetX;
-            group.position.y -= offsetY;
-            group.position.z -= offsetZ;
-          } else if (leftCenter.distanceTo(center2) < 200) {
-            const offsetX = center2.x - leftCenter.x;
-            const offsetY = center2.y - leftCenter.y;
-            const offsetZ = center2.z - leftCenter.z;
-            group.position.x -= offsetX;
-            group.position.y -= offsetY;
-            group.position.z -= offsetZ;
-          } else if (rightCenter.distanceTo(center2) < 200) {
-            const offsetX = center2.x - rightCenter.x;
-            const offsetY = center2.y - rightCenter.y;
-            const offsetZ = center2.z - rightCenter.z;
-            group.position.x -= offsetX;
-            group.position.y -= offsetY;
-            group.position.z -= offsetZ;
-          }
-        });
-
-        // 监听拖动开始事件
-        dragControls2.addEventListener("dragstart", function (event) {
-          // 停止渲染循环，防止拖动过程中产生闪烁
-          controls.enabled = false;
-          dragControls1.enabled = false;
-        });
-
-        // 监听拖动结束事件
-        dragControls2.addEventListener("dragend", function (event) {
-          // 启动渲染循环
-          controls.enabled = true;
-          dragControls1.enabled = true;
+              if (center1.distanceTo(center2) < 200) {
+                const offsetX = center2.x - center1.x;
+                const offsetY = center2.y - center1.y;
+                const offsetZ = center2.z - center1.z;
+                group.position.x -= offsetX;
+                group.position.y -= offsetY;
+                group.position.z -= offsetZ;
+              } else if (leftCenter.distanceTo(center2) < 200) {
+                const offsetX = center2.x - leftCenter.x;
+                const offsetY = center2.y - leftCenter.y;
+                const offsetZ = center2.z - leftCenter.z;
+                group.position.x -= offsetX;
+                group.position.y -= offsetY;
+                group.position.z -= offsetZ;
+              } else if (rightCenter.distanceTo(center2) < 200) {
+                const offsetX = center2.x - rightCenter.x;
+                const offsetY = center2.y - rightCenter.y;
+                const offsetZ = center2.z - rightCenter.z;
+                group.position.x -= offsetX;
+                group.position.y -= offsetY;
+                group.position.z -= offsetZ;
+              }
+            }
+          });
         });
       });
     };
@@ -164,14 +161,6 @@ export default {
     renderer.setClearColor(0x1e1e1e, 1); //设置背景颜色
     renderer.render(scene, camera);
 
-    /*----------------------------创建css2d渲染器对象----------------------------*/
-    const css2DRenderer = new CSS2DRenderer();
-    css2DRenderer.setSize(width, height);
-    css2DRenderer.domElement.style.position = "absolute";
-    css2DRenderer.domElement.style.top = "0px";
-    // 设置.style.pointerEvents = none，就可以解决HTML元素标签对threejs canvas画布鼠标事件的遮挡
-    css2DRenderer.domElement.style.pointerEvents = "none";
-
     // 添加平行光
     addDirectionalLight(scene);
 
@@ -179,19 +168,17 @@ export default {
     const grid = new THREE.GridHelper(5000, 50, 0xc1c1c1, 0x8d8d8d);
     scene.add(grid);
 
-    addAxesHelper(scene);
+    // addAxesHelper(scene);
 
     // 添加交互控制器
     const controls = addOrbitControls(scene, camera, renderer);
 
     const init = () => {
       document.body.appendChild(renderer.domElement);
-      document.body.appendChild(css2DRenderer.domElement);
       loadObj();
       addStats(stats);
       window.onresize = function () {
         renderer.setSize(window.innerWidth, window.innerHeight);
-        css2DRenderer.setSize(window.innerWidth, window.innerHeight);
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
       };
@@ -200,7 +187,6 @@ export default {
     const render = () => {
       stats.update();
       renderer.render(scene, camera);
-      css2DRenderer.render(scene, camera);
       requestAnimationFrame(render);
     };
 
