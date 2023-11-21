8611a882fcdcb5feb0d2eff2c4b4c26bbad9cccd
ZuPerfect <315698477@qq.com>
2023-11-21 17:55:12 +0800

    feat: 补充

---
+++ src/components/FirstWebGL.vue
@@ -0,0 +1,36 @@
+<!--
+ * @描 述: 第一个WebGL shader
+ * @作 者: 朱鹏飞
+-->
+
+<template>
+  <canvas id="webgl" width="500" height="500" style="background: #000"></canvas>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import vertexShaderSource from "../shader/vertexShader.glsl";
+
+export default {
+  name: "FirstWebGL",
+  setup() {
+    const initWebgl = () => {
+      const canvas = document.getElementById("webgl");
+      const gl = canvas.getContext("webgl");
+      gl.drawArrays();
+      gl.createShader();
+      gl.shaderSource();
+      gl.complieShader();
+      gl.createProgram();
+      gl.attachShader();
+      gl.linkProgram();
+      gl.useProgram();
+    };
+    onMounted(() => {
+      initWebgl();
+    });
+    return {};
+  },
+};
+</script>
+<style></style>
---
+++ src/shader/vertexShader.glsl
@@ -0,0 +1,4 @@
+void mian() {
+    gl_PointSize = 20.0;
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+}
